b8975caf969bc0e70033139fe04e23dac1c5b092
fernando <fernando_lopezc@live.com>
2019-02-12 15:43:28 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,13 +2,14 @@ def hello_t(array)
   i = 0
 
   while i < array.length
-    yield array[i]
+    yield array[i] # grabbving the value of each successive index element in our loop. Yielding each to a block. Now we are ready to call our method.
     i = i + 1
   end
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+hello_t(["Tim", "Tom", "Jim"]) do |name| #this will accept the element passed onto yield 
+  # in this case array[i]
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
