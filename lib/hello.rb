e42f541b719b0633aaa36e3a986970aecab364ee
fernando <fernando_lopezc@live.com>
2019-02-12 15:44:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,6 +5,7 @@ def hello_t(array)
     yield array[i] # grabbving the value of each successive index element in our loop. Yielding each to a block. Now we are ready to call our method.
     i = i + 1
   end
+  array
 end
 
 # call your method here!
